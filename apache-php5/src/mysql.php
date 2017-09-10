8ee1899a4cedd016cece091d7d0d8242ad0d70c2
Shock Liang <shockwater@gmail.com>
2017-09-10 19:49:18 +0800

    Add mysql connection and install mysql extendtion via docker file

---
+++ apache-php5/src/mysql.php
@@ -0,0 +1,14 @@
+<?php
+
+ $database = "users";
+ $user = "root";
+ $password = "secret";
+ $host = "mysql";
+
+ $db = mysql_connect("$host", "$user", "$password");
+
+ if (!$db) {
+     echo "Cannot connect to the database server";
+ } elseif ($db && !(mysql_select_db($SQL_DBASE, $db))) {
+     echo "Sucessfully connected to the database server! Database Users selected!";
+ }
