e1e927a32cf3a7f5418d590bebff2c1bf8018d17
Shock Liang <shockwater@gmail.com>
2017-09-11 22:55:44 +0800

    Link to mysql container and modify php container section to rebuild php image via dockerfile

--- nginx-php7/www/html/index.php
+++ nginx-php7/www/html/index.php
@@ -6,4 +6,25 @@
 <body>
     <h1>Hello World! AMAZING!!!</h1>
     <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+    <?php
+     $database ="my_db";
+     $user = "user";
+     $password = "password";
+     $host = "mysql";
+
+     $connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);
+     $query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");
+     $tables = $query->fetchAll(PDO::FETCH_COLUMN);
+
+     if (empty($tables)) {
+         echo "<p>There are no tables in database \"{$database}\".</p>";
+     } else {
+         echo "<p>Database \"{$database}\" has the following tables:</p>";
+         echo "<ul>";
+         foreach ($tables as $table) {
+             echo "<li>{$table}</li>";
+         }
+         echo "</ul>";
+     }
+    ?>
 </body>
